85f0d597ad1ef10287243ba8ab535b08151d6d07
Marco Gartmann <marco.gartmann@ost.ch>
2021-03-25 17:44:05 +0100

    add tests to check and write rst docu 2

--- clang-tools-extra/docs/clang-tidy/checks/misc-cin-cout.rst
+++ clang-tools-extra/docs/clang-tidy/checks/misc-cin-cout.rst
@@ -3,4 +3,43 @@
 misc-cin-cout
 =============
 
-FIXME: Describe what patterns does the check detect and why. Give examples.
+The check diagnoses when a predefined standard stream object (i.e., ``cin``, ``wcin``, 
+``cout``, ``wcout``, ``cerr``, ``wcerr``, ``clog`` and ``wclog``) is used outside the ``main`` function.
+
+For instance, in the following code, the use of ``std::cout`` outside of ``main()`` would get
+flagged whereas the use of ``std::cout`` inside ``main()`` is not flagged:
+
+.. code-block:: c++
+
+  #include <iostream>
+
+  void some_function() { 
+    std::cout << "This triggers the check."; 
+         ~~~~ 
+  } 
+
+  int main() {
+    std::cout << "This does not trigger the check.";
+  }
+
+Since the predefined standard stream objects are global objects, their use outside of ``main()`` worsens a 
+program's testability and is thus discouraged. Instead, those objects should only be used inside ``main()``.
+They can then be passed as arguments to other functions like so:
+
+.. code-block:: c++
+
+  #include <iostream>
+
+  void some_function(std::istream & in, std::ostream & out) { 
+    out << "This does not trigger the check."; 
+
+    int i{0};
+    in >> i;
+  }  
+
+  int main() {
+    some_function(std::cin, std::cout);
+  }
+
+In contrast to using ``std::cin`` and ``std::cout`` directly, in the above example, it is possible to inject 
+mocked stream objects into ``some_function()`` during testing.
--- clang-tools-extra/test/clang-tidy/checkers/misc-cin-cout.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/misc-cin-cout.cpp
@@ -1,14 +1,43 @@
-// RUN: %check_clang_tidy %s misc-cin-cout %t
+// RUN: %check_clang_tidy -std=c++14-or-later %s misc-cin-cout %t
 
 #include <iostream>
 
-std::cout << "This should trigger the check";
-// CHECK-MESSAGES: :[[@LINE-1]]:1: warning: cin/cout should only be used inside main function [misc-cin-cout]
-
 void problematic() {
+  // CHECK-MESSAGES: :[[@LINE+1]]:8: warning: predefined standard stream objects should not be used outside the main function [misc-cin-cout]
+  std::cout << "This should trigger the check";
+
+    // CHECK-MESSAGES: :[[@LINE+1]]:8: warning: predefined standard stream objects should not be used outside the main function [misc-cin-cout]
   std::wcout << "This should trigger the check";
+
+    // CHECK-MESSAGES: :[[@LINE+1]]:8: warning: predefined standard stream objects should not be used outside the main function [misc-cin-cout]
+  std::cerr << "This should trigger the check";
+
+    // CHECK-MESSAGES: :[[@LINE+1]]:8: warning: predefined standard stream objects should not be used outside the main function [misc-cin-cout]
+  std::wcerr << "This should trigger the check";
+
+    // CHECK-MESSAGES: :[[@LINE+1]]:8: warning: predefined standard stream objects should not be used outside the main function [misc-cin-cout]
+  std::clog << "This should trigger the check";
+
+    // CHECK-MESSAGES: :[[@LINE+1]]:8: warning: predefined standard stream objects should not be used outside the main function [misc-cin-cout]
+  std::wclog << "This should trigger the check";
+
+  int i;
+  // CHECK-MESSAGES: :[[@LINE+1]]:8: warning: predefined standard stream objects should not be used outside the main function [misc-cin-cout]
+  std::cin >> i;
+
+  // CHECK-MESSAGES: :[[@LINE+1]]:8: warning: predefined standard stream objects should not be used outside the main function [misc-cin-cout]
+  std::wcin >> i;
 }
 
 int main() {
-  std::cout << "This should not trigger the check";
+  std::cout << "This should not trigger the check";  // OK
+  std::wcout << "This should not trigger the check"; // OK
+  std::cerr << "This should not trigger the check";  // OK
+  std::wcerr << "This should not trigger the check"; // OK
+  std::clog << "This should not trigger the check";  // OK
+  std::wclog << "This should not trigger the check"; // OK
+
+  int i;
+  std::cin >> i;  // OK
+  std::wcin >> i; // OK
 }
