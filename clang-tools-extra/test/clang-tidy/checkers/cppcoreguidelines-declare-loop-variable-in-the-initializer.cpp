75ca1c3fc3ec2b4cef7e3b2f9345dcbcdda8b5d8
Fabian Thurnheer <f.thurnheer7@gmail.com>
2021-04-07 15:24:56 +0200

    execute git clang-format

--- clang-tools-extra/test/clang-tidy/checkers/cppcoreguidelines-declare-loop-variable-in-the-initializer.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/cppcoreguidelines-declare-loop-variable-in-the-initializer.cpp
@@ -1,17 +1,17 @@
 // RUN: %check_clang_tidy %s cppcoreguidelines-declare-loop-variable-in-the-initializer %t
 
 void forLoopFunction() {
-    const int Limit{10};
-    int I{0};
+  const int Limit{10};
+  int I{0};
 
-    // CHECK-MESSAGES: :[[@LINE+1]]:5: warning: Prefer to declare a loop variable in the initializer part of a for-statement [cppcoreguidelines-declare-loop-variable-in-the-initializer]
-    for (; I < Limit; I++) {
-    }
+  // CHECK-MESSAGES: :[[@LINE+1]]:5: warning: Prefer to declare a loop variable in the initializer part of a for-statement [cppcoreguidelines-declare-loop-variable-in-the-initializer]
+  for (; I < Limit; I++) {
+  }
 
-    // CHECK-MESSAGES: :[[@LINE+1]]:5: warning: Prefer to declare a loop variable in the initializer part of a for-statement [cppcoreguidelines-declare-loop-variable-in-the-initializer]
-    for (I = 0; I < Limit; I++) {
-    }
+  // CHECK-MESSAGES: :[[@LINE+1]]:5: warning: Prefer to declare a loop variable in the initializer part of a for-statement [cppcoreguidelines-declare-loop-variable-in-the-initializer]
+  for (I = 0; I < Limit; I++) {
+  }
 
-    for (int I{0}; I < Limit; I++) {
-    }
+  for (int I{0}; I < Limit; I++) {
+  }
 }
