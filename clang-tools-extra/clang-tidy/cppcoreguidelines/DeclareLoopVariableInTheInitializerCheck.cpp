29f2845624e6bfe1e3dc76d54e6d262d2f606f2e
Fabian Thurnheer <f.thurnheer7@gmail.com>
2021-04-21 09:13:00 +0200

    add astVisitor class

--- clang-tools-extra/clang-tidy/cppcoreguidelines/DeclareLoopVariableInTheInitializerCheck.cpp
+++ clang-tools-extra/clang-tidy/cppcoreguidelines/DeclareLoopVariableInTheInitializerCheck.cpp
@@ -8,6 +8,7 @@
 
 #include "DeclareLoopVariableInTheInitializerCheck.h"
 #include "clang/AST/ASTContext.h"
+#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 
 using namespace clang::ast_matchers;
@@ -16,18 +17,90 @@ namespace clang {
 namespace tidy {
 namespace cppcoreguidelines {
 
+class IsOutsideForStmtVisitor
+    : public RecursiveASTVisitor<IsOutsideForStmtVisitor> {
+  friend class RecursiveASTVisitor<IsOutsideForStmtVisitor>;
+
+public:
+  IsOutsideForStmtVisitor(const ForStmt *ForStmt, const VarDecl *VarDecl,
+                          const MatchFinder::MatchResult &Result)
+      : ForLoop(ForStmt), Declaration(VarDecl), externResult(Result),
+        IsOutsideForStmt(false) {}
+
+  bool hasDeclRefExpOutside(const TranslationUnitDecl *TransUnit) {
+    // Find out more about TraverseDecl
+    TraverseDecl(const_cast<TranslationUnitDecl *>(TransUnit));
+    return IsOutsideForStmt;
+  }
+
+private:
+  bool VisitDeclRefExpr(DeclRefExpr *D) {
+    if (const VarDecl *To = dyn_cast<VarDecl>(D->getDecl())) {
+      if (To == Declaration) {
+        IsOutsideForStmt =
+            !IsInsideForLoop(externResult, DynTypedNode::create(*D));
+        if (IsOutsideForStmt) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  // IsInsideMatchedForLoop
+  bool IsInsideForLoop(const MatchFinder::MatchResult &Result,
+                       const DynTypedNode &Node) {
+    const auto *AsForStmt = Node.get<ForStmt>();
+
+    if (AsForStmt && AsForStmt == ForLoop) {
+      return true;
+    }
+
+    return llvm::any_of(Result.Context->getParents(Node),
+                        [&](const DynTypedNode &Parent) {
+                          return IsInsideForLoop(Result, Parent);
+                        });
+  }
+
+  const ForStmt *ForLoop;
+  const VarDecl *Declaration;
+  const MatchFinder::MatchResult &externResult;
+  bool IsOutsideForStmt;
+};
+
 void DeclareLoopVariableInTheInitializerCheck::registerMatchers(
     MatchFinder *Finder) {
-  Finder->addMatcher(forStmt(unless(has(declStmt()))).bind("forStmt"), this);
+  Finder->addMatcher(
+      declRefExpr(
+          hasAncestor(forStmt().bind("ForStmt")), // is in for statement
+          anyOf(hasAncestor(unaryOperator()),     // gets modified
+                hasAncestor(
+                    binaryOperator(hasOperatorName("=")))), // gets modified
+          to(varDecl(unless(hasAncestor(forStmt(equalsBoundNode("ForStmt")))))
+                 .bind("VarDecl"))) // is declared outside a for statement
+          .bind("DeclRefExpr"),
+      this);
 }
 
 void DeclareLoopVariableInTheInitializerCheck::check(
     const MatchFinder::MatchResult &Result) {
-  const auto *MatchedForStmt = Result.Nodes.getNodeAs<ForStmt>("forStmt");
+  const auto *MatchedForStmt = Result.Nodes.getNodeAs<ForStmt>("ForStmt");
+  const auto *MatchedVarDecl = Result.Nodes.getNodeAs<VarDecl>("VarDecl");
+  const auto *MatchedDeclRefExpr =
+      Result.Nodes.getNodeAs<DeclRefExpr>("DeclRefExpr");
+
+  // Maybe just the next Compound Stmt fo the VarDecl?
+  const auto TransUnit = Result.Context->getTranslationUnitDecl();
+
+  if (IsOutsideForStmtVisitor(MatchedForStmt, MatchedVarDecl, Result)
+          .hasDeclRefExpOutside(TransUnit)) {
+    return;
+  }
 
   diag(MatchedForStmt->getBeginLoc(),
-       "Prefer to declare a loop variable in the initializer part of a "
-       "for statement");
+       "The variable %0 gets modified in this for loop statment, but is not "
+       "declared in it. The Variable is also not needed outside the loop.")
+      << MatchedDeclRefExpr->getDecl();
 }
 
 } // namespace cppcoreguidelines
