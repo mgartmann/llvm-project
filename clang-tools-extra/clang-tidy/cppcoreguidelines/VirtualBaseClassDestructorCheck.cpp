45b82cc8e5952fe13f7f1e4f65f234d86f1446bc
Marco Gartmann <marco.gartmann@ost.ch>
2021-05-06 13:17:57 +0200

    remove iostream include

--- clang-tools-extra/clang-tidy/cppcoreguidelines/VirtualBaseClassDestructorCheck.cpp
+++ clang-tools-extra/clang-tidy/cppcoreguidelines/VirtualBaseClassDestructorCheck.cpp
@@ -10,7 +10,6 @@
 #include "clang/AST/ASTContext.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/Lex/Lexer.h"
-#include <iostream>
 #include <string>
 
 using namespace clang::ast_matchers;
